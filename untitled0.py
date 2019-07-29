2b294ee1b2d951f236eae3c6378b8360a3029e90
Ahsanrao237 <ahsanrao237@gmail.com>
2019-07-30 04:01:00 +0500

    assign 1-6, assign1, assign chinesePuzz, modules

---
+++ untitled0.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 02:57:43 2019
+
+@author: Ahsan Ghaffar
+"""
+import sys
+print("hello")
+
+def add (a,b):
+    return a+b;
+
+
+
+#print(sys.argv)
+#print(sys.argv[0])
+#print(sys.argv[1])
+#print(sys.argv[2])
+#rint(sys.argv[3])
+    #sys.argv[0] ye system ke lye hota he is lye use ni krte
+print(add(sys.argv[1],sys.argv[2]))
